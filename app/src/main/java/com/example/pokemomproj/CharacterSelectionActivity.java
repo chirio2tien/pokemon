69253391297557bc0035b350daa7181f2b886801
derlay <bombivulun@gmail.com>
2025-03-17 23:27:42 +0700

    Nhu con cac

--- app/src/main/java/com/example/pokemomproj/CharacterSelectionActivity.java
+++ app/src/main/java/com/example/pokemomproj/CharacterSelectionActivity.java
@@ -1,43 +1,43 @@
 package com.example.pokemomproj;
 
-                    import android.content.Intent;
-                    import android.os.Bundle;
-                    import android.view.View;
-                    import android.widget.ImageView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
-                    import androidx.appcompat.app.AppCompatActivity;
+public class CharacterSelectionActivity extends AppCompatActivity {
 
-                    public class CharacterSelectionActivity extends AppCompatActivity {
+    private final int[] characterImageViewIds = {
+            R.id.character1, R.id.character2, R.id.character3,
+            R.id.character4, R.id.character5, R.id.character6
+    };
 
-                        @Override
-                        protected void onCreate(Bundle savedInstanceState) {
-                            super.onCreate(savedInstanceState);
-                            getSupportActionBar().hide();
-                            setContentView(R.layout.activity_character_selection);
+    private final String[] characterNames = {
+            "giratina", "gardervoid", "incineroar",
+            "urshifu", "pikachu", "psyduck"
+    };
 
-                            ImageView character1 = findViewById(R.id.character1);
-                            ImageView character2 = findViewById(R.id.character2);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_character_selection);
 
-                            character1.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    selectCharacter("giratina");
+        setupCharacterSelections();
+    }
 
-                                }
-                            });
+    private void setupCharacterSelections() {
+        for (int i = 0; i < characterImageViewIds.length; i++) {
+            final String characterName = characterNames[i];
+            ImageView characterImage = findViewById(characterImageViewIds[i]);
+            characterImage.setOnClickListener(v -> selectCharacter(characterName));
+        }
+    }
 
-                            character2.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    selectCharacter("gardervoid");
-                                }
-                            });
-                        }
-
-                        private void selectCharacter(String characterName) {
-                            Intent intent = new Intent(this, MainActivity.class);
-                            intent.putExtra("characterName", characterName);
-                            startActivity(intent);
-                            finish();
-                        }
-                    }
+    private void selectCharacter(String characterName) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("characterName", characterName);
+        startActivity(intent);
+        finish();
+    }
+}
